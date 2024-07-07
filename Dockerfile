144df2263d4111df46a7eb57e5ec53fd29c6c7bb
Vermaas <vermaas@astron.nl>
2024-07-07 11:57:23 +0200

    add Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -14,5 +14,8 @@ CMD ["uvicorn", "api.app:app", "--proxy-headers", "--host", "0.0.0.0", "--port",
 # build the image like this:
 # docker build -t my-energy-connector:latest .
 
+# run the container like this:
+# docker run -d --name my-energy-connector --mount type=bind,source=$HOME/shared,target=/shared -p 8015:8000 --restart always my-energy-connector:latest
+
 # log into the container
 # docker exec -it my-energy-connector sh
--- api/app.py
+++ api/app.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import ORJSONResponse
 from database.energy_db import DB
 
-app = FastAPI(default_response_class=ORJSONResponse,port=8000)
+app = FastAPI(
+    title='my_energy_connector',
+    version="0.9.0",
+    description="backend API for mongo my_energy database",
+    default_response_class=ORJSONResponse,
+    port=8000)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # http://localhost:8000/getseries/?start=2024-06-21&end=2024-06-22&interval=Hour
 @app.get('/{getseries}/')
